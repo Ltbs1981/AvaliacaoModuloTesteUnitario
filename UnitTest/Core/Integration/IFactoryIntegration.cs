b1916c648a491ce47f362dbcf61ae68ee989c64a
Thiago Ramos <thiago.s.ramos@outlook.com>
2023-08-31 15:32:52 -0300

    Add project files.

---
+++ UnitTest/Core/Integration/IFactoryIntegration.cs
@@ -0,0 +1,11 @@
+﻿using UnitTest.Feature.Produto;
+
+namespace UnitTest.Core.Integration
+{
+    public interface IFactoryIntegration
+    {
+        public bool ExistProduct(Produto produto);
+        public void ReservaProduto(Produto produto);
+
+    }
+}
---
+++ UnitTest/Feature/Produto/Contracts/IProdutoRepository.cs
@@ -0,0 +1,10 @@
+﻿using UnitTest.Core;
+
+namespace UnitTest.Feature.Produto.Contracts
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Produto BuscaProdutoPorSKU(string sku);
+        void RegistraPedidoProduto(Produto produto);
+    }
+}
